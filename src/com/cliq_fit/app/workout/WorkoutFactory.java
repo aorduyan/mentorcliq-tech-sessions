5431de3723df41d3e29c4b09a5c70ed3f8476147
unknown <rubina.shakhkyan@mentorcliq.com>
2020-05-14 11:07:48 +0400

    Fixed syntax

--- src/com/cliq_fit/app/workout/WorkoutFactory.java
+++ src/com/cliq_fit/app/workout/WorkoutFactory.java
@@ -1,22 +1,22 @@
 package com.cliq_fit.app.workout;
 import com.cliq_fit.app.workout.stage.exercise*;
 import com.cliq_fit.app.workout.stage.stretch.*;
-import com.cliq_fit.app.workout.stage.warmup.*;
+import com.cliq_fit.app.workout.stage.WarmUp.*;
 
 public class WorkoutFactory {
 
     public static Workout buildWorkout(WorkoutType workoutType) {
         switch (workoutType) {
             case SWIMMING_RELAX:
-                return new Workout(new WarmupStageShoulder(), new ExerciseStageYoga(), new StretchStageLegs());
+                return new Workout(new WarmUpStageShoulder(), new ExerciseStageYoga(), new StretchStageLegs());
             case SWIMMING_CHAMPION:
-                return new Workout(new WarmupStageShoulder(), new ExerciseStageSwimmingAdvanced(), new StretchStageLegs());
+                return new Workout(new WarmUpStageShoulder(), new ExerciseStageSwimmingAdvanced(), new StretchStageLegs());
             case BOXING:
-                return new Workout(new WarmupStageLegs(), new ExerciseStageShoulder(), new StretchStageShoulder());
+                return new Workout(new WarmUpStageLegs(), new ExerciseStageShoulder(), new StretchStageShoulder());
             case RUNNING:
-                return new Workout(new WarmupStageLegs(), new ExerciseStageLegs(), new StretchStageLegs());
+                return new Workout(new WarmUpStageLegs(), new ExerciseStageLegs(), new StretchStageLegs());
             case DOESNT_MAKE_SENSE_COMBINATION:
-                return new Workout(new WarmupStageAbs(), new ExerciseStageYoga(), new StretchStageShoulders());
+                return new Workout(new WarmUpStageAbs(), new ExerciseStageYoga(), new StretchStageShoulders());
             default:
                 throw new RuntimeException("Unknown workout type = " + workoutType);
         }
